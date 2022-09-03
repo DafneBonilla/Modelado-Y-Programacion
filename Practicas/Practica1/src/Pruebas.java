af02434b39837a32c50f58b52cac4a48867d8cb3
Camilo <jcamilo@ciencias.unam.mx>
2022-09-03 13:19:39 -0500

    cambios a como compilar

--- Practicas/Practica1/src/Pruebas.java
+++ Practicas/Practica1/src/Pruebas.java
@@ -4,36 +4,12 @@ public class Pruebas {
 
     public static void main(String[] args) {
 
-        Fighter p1 = new Korby();
-        Fighter p2 = new MeganMan();
-        
-        System.out.println(p1.getBlock());
-
-        System.out.println(p1.getName());
-
-        System.out.println(p1.getSkillName());
-
-        //p1.transform();
-
-        System.out.println(p1.getSkillName());
-
-        p2.defend();
-
-        System.out.println(p2.getHP());
-
-        System.out.println(p2.getBlock());
-
-        System.out.println(p1.hit(p2));
-
-        System.out.println(p2.getHP());
-
-        
-
-        System.out.println(p2.isAlive());
-
-        System.out.println(p1.hit(p2));
-
         int random = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+        System.out.println(random);
 
     }
 }
+
+// Desde Practica 1
+// javac -d classes src/*.java
+// java -cp classes Pruebas
