3ecb9f30d59326876fadae39484709202987941c
Camilo <jcamilo@ciencias.unam.mx>
2022-09-05 14:00:00 -0500

    primeras dos documentaciones xd

--- Practicas/Practica1/src/BoomerangSkill.java
+++ Practicas/Practica1/src/BoomerangSkill.java
@@ -1,5 +1,14 @@
+/**
+ * Class to represent the boomerang skill of the fighter Korby
+ * The boomerang skill has a damage of 10 and a defense of 30
+ */
 public class BoomerangSkill extends SpSkill<Korby> {
 
+    /**
+     * Constructor of the boomerang skill
+     * 
+     * @param owner the owner of the skill
+     */
     public BoomerangSkill(Korby owner) {
         atkBoost = 10;
         defBoost = 30;
@@ -7,16 +16,33 @@ public class BoomerangSkill extends SpSkill<Korby> {
         this.owner = owner;
     }
 
+    /**
+     * Calculates and returns the damage that the skill will do to the target
+     * 
+     * @param target the target of the hit
+     * @return the damage that the skill will do to the target
+     */
     @Override
     public int hit(Fighter target) {
         return atkBoost - target.getBlock();
     }
 
+    /**
+     * Calculates and returns the defense that the skill will give to the owner
+     * 
+     * @return the defense that the skill will give to the owner
+     */
     @Override
     public int defend() {
         return (int) (defBoost * 1.5);
     }
 
+    /**
+     * Returns a description of the skill, this description contains the name, the
+     * attack boost, the defense boost, and the special effect
+     * 
+     * @return a description of the skill
+     */
     @Override
     public String getDescription() {
         return "Nombre: " + name + " Atk: " + atkBoost + " Def: " + defBoost + " Al defender ganas "
